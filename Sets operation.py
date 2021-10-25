3e89975ac6d9e5bed29ba0351f3ad12474c74b7b
Himanshu-8830 <82021925+Himanshu-8830@users.noreply.github.com>
2021-10-25 20:07:59 +0530

    Add files via upload

---
+++ Sets operation.py
@@ -0,0 +1,15 @@
+
+H = {0, 2, 4, 6, 8};
+S = {1, 2, 3, 4, 5};
+
+# set union
+print("Union of H and S is",H | S)
+
+# set intersection
+print("Intersection of H and S is",H & S)
+
+# set difference
+print("Difference of H and S is",H - S)
+
+# set symmetric difference
+print("Symmetric difference of H and S is",H ^ S)
